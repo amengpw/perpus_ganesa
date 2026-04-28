5f2c5147f6bcaabdf8947bab5b54e9a9f51cba48
zunaizah <zunaizah314@gmail.com>
2026-04-28 21:38:58 +0700

    Setup PostgreSQL, Gunicorn, dan Whitenoise untuk Render

--- core/settings.py
+++ core/settings.py
@@ -82,10 +82,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': dj_database_url.parse('postgresql://postgres:ganesa1357909@db.lzbjueliveotpgosuuzc.supabase.co:5432/postgres'),
+    
 }
 
 
